2f2a8bf80009ff76b3d97662c17aadc51d91f149
on21FU <flo.uhlschmiedmx.de>
2022-11-08 22:50:13 +0100

    build calender

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
+import './calendar';
 
 import Alpine from 'alpinejs';
+import { test } from './calendar';
+test();
+console.log("hallo");
 
 window.Alpine = Alpine;
+
 
 Alpine.start();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,28 @@
 
 <x-app-layout>
     <style>
-        .parent {
+        .calendarGridParent {
+            margin-top: 20px;
             display: grid;
             grid-template-columns: repeat(7, 1fr);
             grid-template-rows: repeat(6, 1fr);
             grid-column-gap: 0px;
             grid-row-gap: 0px;
             }
+        .calendarGridParent div {
+            width: 45px;
+            height: 45px;
+            border: 1px groove rgba(0,0,0,0.1);
+            background-color: rgba(8,214,214,0.1);
+            padding-top: 5px;
+        }
+        .weekday {
+            background-color: #08D6D6 !important; 
+            font-weight: bold;
+        }
+        .prev-day {
+            color: rgba(0,0,0,0.3);
+        }
     </style>
     <div class="container mt-5">
         <div class="row">
@@ -31,50 +46,52 @@
                 <option value="2025">2025</option>
             </select>
         </div>
-        <div class="parent">
-            <div>Monday</div>
-            <div>Tuesday</div>
-            <div>Wednesday</div>
-            <div>Thursday</div>
-            <div>Friday</div>
-            <div>Saturday</div>
-            <div>Sunday</div>
-            <div class="prev-day">31</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>6</div>
-            <div>7</div>
-            <div>8</div>
-            <div>9</div>
-            <div>10</div>
-            <div>11</div>
-            <div>12</div>
-            <div>13</div>
-            <div>14</div>
-            <div>15</div>
-            <div>16</div>
-            <div>17</div>
-            <div>18</div>
-            <div>19</div>
-            <div>20</div>
-            <div>21</div>
-            <div>22</div>
-            <div>23</div>
-            <div>24</div>
-            <div>25</div>
-            <div>26</div>
-            <div>27</div>
-            <div>28</div>
-            <div>29</div>
-            <div>30</div>
-            <div class="prev-day">1</div>
-            <div class="prev-day">2</div>
-            <div class="prev-day">3</div>
-            <div class="prev-day">4</div>
+        <div class="row justify-content-center">
+            <div class="col-3 text-center calendarGridParent">
+                <div class="weekday">Mon</div>
+                <div class="weekday">Tue</div>
+                <div class="weekday">Wed</div>
+                <div class="weekday">Thu</div>
+                <div class="weekday">Fri</div>
+                <div class="weekday">Sat</div>
+                <div class="weekday">Sun</div>
+                <div class="prev-day">31</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
+                <div>19</div>
+                <div>20</div>
+                <div>21</div>
+                <div>22</div>
+                <div>23</div>
+                <div>24</div>
+                <div>25</div>
+                <div>26</div>
+                <div>27</div>
+                <div>28</div>
+                <div>29</div>
+                <div>30</div>
+                <div class="prev-day">1</div>
+                <div class="prev-day">2</div>
+                <div class="prev-day">3</div>
+                <div class="prev-day">4</div>
+            </div>
         </div>
-        
     </div>
+    
 </x-app-layout>
